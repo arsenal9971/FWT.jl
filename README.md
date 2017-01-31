fd7973365772cc45cfabf835e9ac7ed61d761381
Hector Andrade Loarca <arsenal997@gmail.com>
2017-01-31 16:48:33 +0100

    Remodeling of Library

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# FWT
+## FWT.jl
+
+
+FWT is intended to be an alternative to the usual wavelet library Wavelets.jl
--- src/FWT.jl
+++ src/FWT.jl
@@ -1,5 +1,21 @@
+# This file is part of the FWT.jl package; by Héctor Andrade Loarca
+
 module FWT
 
-# package code goes here
+using Images
+using Wavelets
+using PyPlot
+
+export WT,filt_gen, resize_image,load_image, imageplot, rescale,
+       plot_wavelet, subsampling, upsampling, cconvol, reverse, clamp,
+       perform_wavortho_transform, snr
+
+
+
+include("filters.jl")
+include("operations.jl")
+include("imaging.jl")
+
+#include("")
 
 end # module
